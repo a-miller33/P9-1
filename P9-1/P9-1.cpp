08c052d0424eb308f0a2a678ae13d8dbff918417
Adam Miller <a-miller.33@onu.edu>
2024-10-31 12:45:45 -0400

    Order of appearance

--- P9-1/P9-1.cpp
+++ P9-1/P9-1.cpp
@@ -1,2 +1,33 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+
+void initializeArray(int array[], int size);
+
+const int SIZE = 10;
+int randomArray[SIZE];
+
+int main() {
+	initializeArray(randomArray, SIZE);
+
+	cout << "The random integers: ";
+	for (int i = 0; i < SIZE; i++) {
+		cout << randomArray[i] << "  ";
+	}
+
+	
+
+	
+
+
+
+	return 0;
+}
+
+void initializeArray(int array[], int size) {
+	srand(time(0));
+	for (int i = 0; i < SIZE; i++) {
+		array[i] = rand() % 9000 + 1000;
+	}
+}
